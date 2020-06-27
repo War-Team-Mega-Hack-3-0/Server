0bd0ca1b30f3091963988fa47683d37b94d29982
Joao Lucas <jlcarruda3@gmail.com>
2020-06-27 16:05:13 -0300

    feat: implemented the DocumetDBConnector to middleware main app pipe

--- src/header.js
+++ src/header.js
@@ -4,10 +4,14 @@ const express = require('express')
 const helmet = require('helmet')
 const bodyParser = require('body-parser')
 
+const DocumentDBConnector = require('./middlewares/docdb-connector')
+
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(helmet())
+
+app.use(DocumentDBConnector)
 
 module.exports = { app, serverless }
