8093c4ecc080a7f954464034b0bc1f221a163674
Joao Lucas <jlcarruda3@gmail.com>
2020-06-27 16:46:21 -0300

    feat: configured the husky + eslint/prettier for code style enforcement

--- src/handlers/hello.js
+++ src/handlers/hello.js
@@ -1,10 +1,10 @@
 'use strict';
-const { app, serverless } = require('../header')
+const { app, serverless } = require('../header');
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   return res.status(200).json({
     message: 'Go Serverless v1.0! Your function executed successfully!',
-  })
-})
+  });
+});
 
-module.exports.handler = serverless(app)
+module.exports.handler = serverless(app);
