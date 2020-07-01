47b4ab7f50bbb2b3f017aae6a890e0ef1e3750a8
Joao Lucas <jlcarruda3@gmail.com>
2020-07-01 13:40:19 -0300

    improvement: now the encrypter uses a random IV for each encryption

--- src/utils/encrypter.js
+++ src/utils/encrypter.js
@@ -1,16 +1,16 @@
-const { createCipheriv, createDecipheriv } = require('crypto');
+const { createCipheriv, createDecipheriv, randomBytes } = require('crypto');
 const { Buffer } = require('buffer');
 
 const algorithm = 'aes-256-ctr';
 const i = 'utf8';
 const o = 'hex';
 const k = process.env.ENCRYPT_KEY;
-const encryptIv = process.env.ENCRYPT_IV;
+
 const kString = k.slice(0, 32);
 
 module.exports.encrypt = (value) => {
-  const iv = Buffer.from(encryptIv);
-  const ivString = iv.slice(0, 16);
+  const iv = Buffer.from(randomBytes(16));
+  const ivString = iv.toString(o).slice(0, 16);
   const cipher = createCipheriv(algorithm, kString, ivString);
   let crypted = cipher.update(value, i, o);
   crypted += cipher.final(o);
@@ -18,8 +18,8 @@ module.exports.encrypt = (value) => {
 };
 
 module.exports.decrypt = (encrypted) => {
-  const [, crypted] = encrypted.split(':');
-  const ivString = Buffer.from(encryptIv).slice(0, 16);
+  const [iv, crypted] = encrypted.split(':');
+  const ivString = iv.toString(o);
   const decipher = createDecipheriv(algorithm, kString, ivString);
   let decrypted = decipher.update(crypted, o, i);
   decrypted += decipher.final(i);
