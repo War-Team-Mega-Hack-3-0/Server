97e92e56db2ecc591508e8ef4dcb1de99262b5d0
Joao Lucas <jlcarruda3@gmail.com>
2020-07-03 19:34:47 -0300

    feat: added the profile verification for the signin

--- src/handlers/signin.js
+++ src/handlers/signin.js
@@ -1,18 +1,37 @@
 'use strict';
+const bcrypt = require('bcrypt');
+
 const app = require('../app');
 const handler = require('../handler');
 const { sign } = require('../utils/jwt');
 const authenticate = require('../middlewares/authenticate');
+const { AuthenticationRequiredError } = require('common-errors');
+
+const Profile = require('../models/profile');
 
 const DatabaseConnector = require('../middlewares/database-connector');
 
 app.use(DatabaseConnector());
 
-app.post('/signin', authenticate, (req, res) => {
-  const { email } = req.body;
-
+app.post('/signin', authenticate, async (req, res, next) => {
+  const { email, password } = req.body;
+  const error = new AuthenticationRequiredError('Invalid Credentials');
   try {
-    const token = sign({ email });
+    const profile = await Profile.findOne({
+      email,
+    });
+
+    if (!profile) {
+      next(error);
+    }
+
+    const passwordMatches = await bcrypt.compare(password, profile.password);
+
+    if (!passwordMatches) {
+      next(error);
+    }
+
+    const token = sign({ id: profile._id });
     return res.status(200).json({ auth: true, token });
   } catch (error) {
     res.status(401).json({ auth: false });
