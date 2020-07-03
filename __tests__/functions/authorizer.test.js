ed8edf9f1e1dc7bed7cbf62aa89a93dbe5480979
Joao Lucas <jlcarruda3@gmail.com>
2020-07-02 23:42:42 -0300

    feat: added test with invalid key

--- __tests__/functions/authorizer.test.js
+++ __tests__/functions/authorizer.test.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const fs = require('fs');
 
 const { handler } = require('../../src/handlers/authorizer');
-const publicKey = fs.readFileSync('./certificates/jwt-public.key');
+
 const privateKey = fs.readFileSync('./certificates/jwt-private.key');
 const invalidKey = fs.readFileSync('./__tests__/utils/jwt-private-invalid.key');
 
@@ -10,10 +10,10 @@ const data = { email: 'test@email.com' };
 const options = { algorithm: 'RS512' };
 
 const validToken = jwt.sign(data, privateKey, options);
-// const invalidSecretToken = jwt.sign(data, invalidKey, options)
+const invalidSecretToken = jwt.sign(data, invalidKey, options);
 
 describe('Authorizer', () => {
-  it('should successfully authenticate if JWT passed is signed correctly', async () => {
+  it('should successfully authorize if JWT passed is signed correctly', async () => {
     const event = { authorizationToken: validToken, methodArn: 'testArn' };
 
     const expectedStatement = [
@@ -38,5 +38,31 @@ describe('Authorizer', () => {
     expect(principalId).toBeDefined();
     expect(policyDocument).toBeDefined();
     expect(policyDocument.Statement).toStrictEqual(expectedStatement);
+  });
+
+  it('should not authorize if JWT is signed with a different key', () => {
+    const event = {
+      authorizationToken: invalidSecretToken,
+      methodArn: 'testArn',
+    };
+
+    const expectedStatement = [
+      {
+        Action: 'execute-api:Invoke',
+        Effect: 'Deny',
+        Resource: event.methodArn,
+      },
+    ];
+    const callback = jest.fn((err, policy) => {
+      err, policy;
+    });
+
+    handler(event, null, callback);
+
+    const { calls } = callback.mock;
+    const [err] = calls[0];
+
+    expect(calls.length).toBe(1);
+    expect(err).toBe('Invalid Token'); // Error to be Null
   });
 });
