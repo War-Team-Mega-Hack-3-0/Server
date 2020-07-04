842fb6aed9bb706a70f3ced0ffdcbbc210487c32
Joao Lucas <jlcarruda3@gmail.com>
2020-07-04 17:30:05 -0300

    improvement: implemented the output filter for signin route

--- src/handlers/signin.js
+++ src/handlers/signin.js
@@ -14,7 +14,7 @@ const DatabaseConnector = require('../middlewares/database-connector');
 app.use(DatabaseConnector());
 
 app.post('/profile/token', authenticate, async (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password: passwd } = req.body;
   const error = new AuthenticationRequiredError('Invalid Credentials');
   try {
     const profile = await Profile.findOne({
@@ -25,14 +25,16 @@ app.post('/profile/token', authenticate, async (req, res, next) => {
       next(error);
     }
 
-    const passwordMatches = await bcrypt.compare(password, profile.password);
+    const passwordMatches = await bcrypt.compare(passwd, profile.password);
 
     if (!passwordMatches) {
       next(error);
     }
-
+    const { password, ...response } = profile;
     const token = sign({ id: profile._id });
-    return res.status(200).json({ auth: true, token });
+    return res
+      .status(200)
+      .json({ auth: true, data: response, meta: { token } });
   } catch (error) {
     res.status(401).json({ auth: false });
   }
