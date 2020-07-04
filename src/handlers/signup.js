1bda5bbc9a2bd7cbed2f4c5a8cdbc2a5d6881496
Joao Lucas <jlcarruda3@gmail.com>
2020-07-04 12:29:37 -0300

    feat: handler for signup lambda

---
+++ src/handlers/signup.js
@@ -0,0 +1,42 @@
+'use strict';
+const { MongoError } = require('mongodb');
+
+const app = require('../app');
+const handler = require('../handler');
+const { sign } = require('../utils/jwt');
+const { AlreadyInUseError } = require('common-errors');
+
+const Profile = require('../models/profile');
+
+const DatabaseConnector = require('../middlewares/database-connector');
+
+app.use(DatabaseConnector());
+
+app.post('/profile/signup', async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const profile = await Profile.create({
+      email,
+      password,
+    });
+
+    if (profile) {
+      const token = sign({ id: profile._id });
+      return res.status(200).json({ data: profile, meta: { token } });
+    }
+    console.error('Profile cant be created: ', profile);
+    res.status(500).send();
+  } catch (error) {
+    if (
+      error instanceof MongoError &&
+      error.message.search('duplicate key error') >= 0
+    ) {
+      return next(new AlreadyInUseError());
+    }
+
+    res.status(500).send();
+  }
+});
+
+module.exports.handler = handler(app);
