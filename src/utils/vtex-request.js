e9ad25812aa4fbafeb43ef27b1f85607cbb3d56b
Joao Lucas <jlcarruda3@gmail.com>
2020-07-05 16:47:19 -0300

    feat: added vtex-request with axios

--- src/utils/vtex-request.js
+++ src/utils/vtex-request.js
@@ -1,8 +1,45 @@
 const axios = require('axios');
+const { ArgumentError } = require('common-errors');
 
-module.exports.vtexRequest = (
+const requestTypeMap = {
+  Orders: { url: '/api/oms/pvt/orders', method: 'GET' },
+};
+
+module.exports.vtexRequest = async ({
   userName,
-  environmentName,
+  environment,
   apiKey,
-  apiToken
-) => {};
+  apiToken,
+  requestType,
+  data,
+  params,
+}) => {
+  const request = requestTypeMap[requestType];
+  if (!request) return Promise.reject(new ArgumentError('requestType'));
+
+  const { url, method } = request;
+
+  let baseURL = `https:${userName}.${environment}.com.br`;
+
+  const axiosConfig = {
+    baseURL,
+    method,
+    url,
+    data,
+    params,
+    headers: {
+      'x-vtex-api-appkey': apiKey,
+      'x-vtex-api-apptoken': apiToken,
+    },
+  };
+
+  try {
+    const response = await axios(axiosConfig);
+    return response;
+  } catch (error) {
+    console.error('Error while requesting resources from VTEX Api', error);
+    return Promise.reject(
+      new Error('Error while requesting resources from VTEX Api')
+    );
+  }
+};
