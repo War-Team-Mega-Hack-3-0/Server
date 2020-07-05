364910afba43a2977017bff6a5e18f35aa934527
Joao Lucas <jlcarruda3@gmail.com>
2020-07-05 00:08:50 -0300

    fix: added cors module for express cors configuration

--- src/app.js
+++ src/app.js
@@ -3,8 +3,11 @@ const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const errors = require('common-errors');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
